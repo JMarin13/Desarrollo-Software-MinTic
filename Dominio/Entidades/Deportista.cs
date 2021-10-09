87aaa576b1d2f0b2fc18c0e815b2f123e94a9f30
David089755 <90647550+David089755@users.noreply.github.com>
2021-10-08 22:56:53 -0500

    Cambio de Nombres por Nombre

--- Dominio/Entidades/Deportista.cs
+++ Dominio/Entidades/Deportista.cs
@@ -7,7 +7,7 @@ namespace Dominio
     {
         public int Id {get; set;}
         public string Documento {get; set;}
-        public string Nombres {get; set;}
+        public string Nombre {get; set;}
         public string Apellidos {get; set;}
         public string Direccion {get; set;}
         public string Celular {get; set;}
