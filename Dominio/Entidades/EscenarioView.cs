4e3291d8cd4f69da2b8e58629c1786e9dae2056c
David089755 <90647550+David089755@users.noreply.github.com>
2021-10-17 17:04:52 -0500

    Create EscenarioView.cs

---
+++ Dominio/Entidades/EscenarioView.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace Dominio
+{
+    public class EscenarioView
+    {
+        public int Id{get;set;}
+        public string Nombre{get;set;}
+        public string Direccion{get;set;}
+        public string Telefono{get;set;}
+        public string Torneo{get;set;}
+       
+    }
+}
