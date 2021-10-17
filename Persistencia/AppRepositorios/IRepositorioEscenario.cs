5e28dfa3974638dd20f5a9283ccbf87802c2eef7
David089755 <90647550+David089755@users.noreply.github.com>
2021-10-17 17:01:01 -0500

    Update IRepositorioEscenario.cs

--- Persistencia/AppRepositorios/IRepositorioEscenario.cs
+++ Persistencia/AppRepositorios/IRepositorioEscenario.cs
@@ -11,5 +11,6 @@ namespace Persistencia
         bool EliminarEscenario(int idEscenario);
         bool ActualizarEscenario(Escenario escenario);
         IEnumerable<Escenario> ListarEscenarios();
+        List<Escenario> ListarEscenarios1();
     }
 }
